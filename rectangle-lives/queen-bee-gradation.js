cd64f1af0911723abf26055f934806ef8f66d435
FAL <falworks.contact@gmail.com>
2018-05-05 03:53:26 +0900

    Add and change patterns.

---
+++ rectangle-lives/queen-bee-gradation.js
@@ -0,0 +1,18 @@
+rectangleLives(
+  "./assets/queen-bee-turn.rle",
+  undefined,
+  {
+    alive: {
+      red: 240,
+      green: 240,
+      blue: 240,
+    },
+    dying: {
+      red: (ratio, yIndex, yIndexMax) => { return (1 - yIndex / yIndexMax) * (248 - ratio * 244); },
+      green: (ratio, yIndex, yIndexMax) => { return 4; },
+      blue: (ratio, yIndex, yIndexMax) => { return (yIndex / yIndexMax) * (248 - ratio * 244); },
+    },
+    background: [4, 4, 4],
+  },
+  20
+);
--- rectangle-lives/runSketch2.js
+++ rectangle-lives/runSketch2.js
@@ -1,18 +1,1 @@
-rectangleLives(
-  "./assets/vacuum-cleaner.rle",
-  undefined,
-  {
-    alive: {
-      red: 240,
-      green: 224,
-      blue: 232,
-    },
-    dying: {
-      red: (ratio) => { return 224 - ratio * 240; },
-      green: (ratio) => { return 4; },
-      blue: (ratio) => { return 64 - ratio * 60; },
-    },
-    background: [4, 4, 4],
-  },
-  20
-);
+rectangleLives("./assets/vacuum-cleaner.rle");
