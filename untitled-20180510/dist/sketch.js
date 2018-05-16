33194ca862a68f4ddc825f79da7a320a68f01c95
FAL <falworks.contact@gmail.com>
2018-05-17 00:41:59 +0900

    Change to instance mode.

--- untitled-20180510/dist/sketch.js
+++ untitled-20180510/dist/sketch.js
@@ -7,73 +7,98 @@
  * @license CC-BY-SA-3.0
  */
 
-let canvas;
-let canvasScaleFactor;
-let drawingColor;
+const htmlElementId = 'Untitled';
 
-function addLine(position, velocity) {
-	velocity.setMag(20 + random(60));
-	position.add(velocity);
-	vertex(position.x, position.y);
-}
+const sketch = (p) => {
+	const nonScaledCanvasSize = 640;
+	let canvas;
+	let canvasScaleFactor;
+	let drawingColor;
 
-function addCurve(position, velocity) {
-	const directionChange = (random(1) < 0.5 ? 1 : -1) * (random(1) < 0.7 ? HALF_PI : QUARTER_PI);
-	const curveResolution = 50;
-	velocity.setMag(0.5);
-	for (let i = 0; i < curveResolution; i += 1) {
-		velocity.rotate(directionChange / curveResolution);
+	function addLine(position, velocity) {
+		velocity.setMag(20 + p.random(60));
 		position.add(velocity);
-		vertex(position.x, position.y);
+		p.vertex(position.x, position.y);
 	}
-}
 
-function drawSomething() {
-	const position = createVector(random(0.3, 0.7) * width, random(0.3, 0.7) * height);
-	const velocity = p5.Vector.fromAngle(Math.floor(random(8)) * QUARTER_PI);
-	stroke(drawingColor);
-	noFill();
-	beginShape();
-	vertex(position.x, position.y);
-	addLine(position, velocity);
-	for (let i = 0, len = Math.floor(random(4)); i < len; i += 1) {
-		addCurve(position, velocity);
+	function addCurve(position, velocity) {
+		const directionChange = (p.random(1) < 0.5 ? 1 : -1) * (p.random(1) < 0.7 ? p.HALF_PI : p.QUARTER_PI);
+		const curveResolution = 50;
+		velocity.setMag(0.5);
+		for (let i = 0; i < curveResolution; i += 1) {
+			velocity.rotate(directionChange / curveResolution);
+			position.add(velocity);
+			p.vertex(position.x, position.y);
+		}
+	}
+
+	function drawSomething() {
+		const position = p.createVector(p.random(0.3, 0.7) * nonScaledCanvasSize, p.random(0.3, 0.7) * nonScaledCanvasSize);
+		const velocity = p5.Vector.fromAngle(Math.floor(p.random(8)) * p.QUARTER_PI);
+		p.stroke(drawingColor);
+		p.noFill();
+		p.beginShape();
+		p.vertex(position.x, position.y);
 		addLine(position, velocity);
+		for (let i = 0, len = Math.floor(p.random(4)); i < len; i += 1) {
+			addCurve(position, velocity);
+			addLine(position, velocity);
+		}
+		p.endShape();
 	}
-	endShape();
-}
 
-function drawDot() {
-  noStroke();
-	fill(drawingColor);
-	ellipse(random(0.3, 0.7) * width, random(0.3, 0.7) * height, 10, 10);
-}
-
-function setup() {
-	const canvasSize = Math.min(windowWidth, windowHeight);
-	canvas = createCanvas(canvasSize, canvasSize);
-	canvasScaleFactor = canvasSize / 640;
-
-	noLoop();
-	drawingColor = color(80);
-	strokeWeight(5);
-}
-
-function draw() {
-	background(252, 255, 248);
-	scale(canvasScaleFactor);
-
-	for (let i = 0; i < 2; i += 1) drawSomething();
-	
-	drawDot();
-}
-
-function mouseClicked() {
-	redraw();
-}
-
-function keyPressed() {
-	if (keyCode === ENTER) {
-		saveCanvas(canvas, 'something', 'png');
+	function drawDot() {
+		p.noStroke();
+		p.fill(drawingColor);
+		p.ellipse(p.random(0.3, 0.7) * nonScaledCanvasSize, p.random(0.3, 0.7) * nonScaledCanvasSize, 10, 10);
 	}
+
+	function getMaxCanvasRegion() {
+		const maxCanvasRegion = {
+			width: p.windowWidth,
+			height: p.windowHeight
+		};
+
+		const node = document.getElementById(htmlElementId);
+
+		if (!node) return maxCanvasRegion;
+
+		const containerRect = node.getBoundingClientRect();
+		maxCanvasRegion.width = containerRect.width;
+		maxCanvasRegion.height = containerRect.height;
+
+		return maxCanvasRegion;
+	}
+
+	p.setup = () => {
+		const maxCanvasRegion = getMaxCanvasRegion();
+		const canvasSize = Math.min(maxCanvasRegion.width, maxCanvasRegion.height);
+		canvas = p.createCanvas(canvasSize, canvasSize);
+		canvasScaleFactor = canvasSize / nonScaledCanvasSize;
+
+		p.noLoop();
+		drawingColor = p.color(80);
+		p.strokeWeight(5);
+	};
+
+	p.draw = () => {
+		p.background(252, 255, 248);
+		p.scale(canvasScaleFactor);
+
+		for (let i = 0; i < 2; i += 1) drawSomething();
+
+		drawDot();
+	};
+
+	p.mouseClicked = () => {
+		p.redraw();
+	};
+
+	p.keyPressed = () => {
+		if (p.keyCode === p.ENTER) {
+			p.saveCanvas(canvas, 'untitled-20180510', 'png');
+		}
+	};
 }
+
+new p5(sketch, htmlElementId);
