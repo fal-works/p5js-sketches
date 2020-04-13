3272fb10b6bde2d7502dbf7d415742b0043a777c
FAL <falworks.contact@gmail.com>
2020-04-13 22:18:24 +0900

    update TEMPLATE

--- TEMPLATE/src/main.ts
+++ TEMPLATE/src/main.ts
@@ -1,88 +1,19 @@
 /**
- * -----------------------------------------------------------------------------
- * @module main
+ * ---- Main ------------------------------------------------------------------
  */
 
-import p5 from "p5";
+import { startSketch } from "@fal-works/p5-extension";
 
-import { startSketch } from "./common/p5util/main";
-import { p, canvas } from "./common/p5util/shared";
-
-import { pauseOrResume } from "./common/p5util/pause";
-import { createPixels, replacePixels } from "./common/p5util/pixels";
-import { drawTranslatedAndRotated } from "./common/p5util/transform";
-
-import { HTML_ELEMENT, LOGICAL_CANVAS_SIZE } from "./settings";
-import * as constants from "./constants";
-
-// ---- variables | functions ----
-
-let drawBackground: () => void;
-
-// ---- reset & initialize ----
-
-const reset = (): void => {};
-
-const initialize = (): void => {
-  const backgroundColor = p.color(constants.BACKGROUND_COLOR);
-  const backgroundPixels = createPixels(() => p.background(backgroundColor));
-  drawBackground = replacePixels.bind(null, backgroundPixels);
-
-  p.noStroke();
-  p.fill(constants.SQUARE_COLOR);
-  p.rectMode(p.CENTER);
-
-  reset();
-};
-
-// ---- draw ----
-
-const drawSquare = () => p.square(0, 0, constants.SQUARE_SIZE);
-
-const drawSketch = (): void => {
-  const center = canvas.logicalCenterPosition;
-  drawTranslatedAndRotated(
-    drawSquare,
-    center.x,
-    center.y,
-    p.frameCount * constants.ROTATION_SPEED
-  );
-};
-
-const draw = (): void => {
-  drawBackground();
-  canvas.drawScaled(drawSketch);
-};
-
-// ---- UI ----
-
-const mousePressed = (): void => {};
-
-const keyTyped = (): void => {
-  switch (p.key) {
-    case "p":
-      pauseOrResume();
-      break;
-    case "s":
-      p.save("image.png");
-      break;
-    case "r":
-      reset();
-      break;
-  }
-};
+import { canvas } from "./common";
+import { HTML_ELEMENT_ID, LOGICAL_CANVAS_HEIGHT } from "./settings";
+import * as sketch from "./sketch";
 
 // ---- start sketch ----
 
-const setP5Methods = (p: p5): void => {
-  p.draw = draw;
-  p.mousePressed = mousePressed;
-  p.keyTyped = keyTyped;
-};
-
 startSketch({
-  htmlElement: HTML_ELEMENT,
-  logicalCanvasSize: LOGICAL_CANVAS_SIZE,
-  initialize,
-  setP5Methods
+  htmlElement: HTML_ELEMENT_ID,
+  logicalCanvasHeight: LOGICAL_CANVAS_HEIGHT,
+  initialize: sketch.initialize, // delete?
+  windowResized: () => canvas.resizeIfNeeded(),
+  p5Methods: sketch.p5Methods,
 });
--- TEMPLATE/src/settings.ts
+++ TEMPLATE/src/settings.ts
@@ -1,25 +1,9 @@
 /**
- * -----------------------------------------------------------------------------
- * @module settings
+ * ---- Settings --------------------------------------------------------------
  */
 
-import { getElementOrBody } from "./common/environment";
-import { RectangleSize } from "./common/data-types";
-
-/**
- * The id of the HTML element to which the canvas should belong.
- */
+/** The id of the HTML element to which the canvas should belong. */
 export const HTML_ELEMENT_ID = "Template";
 
-/**
- * The HTML element to which the canvas should belong.
- */
-export const HTML_ELEMENT = getElementOrBody(HTML_ELEMENT_ID);
-
-/**
- * The logical size of the canvas.
- */
-export const LOGICAL_CANVAS_SIZE: RectangleSize = {
-  width: 800,
-  height: 800
-};
+/** The logical height of the canvas. */
+export const LOGICAL_CANVAS_HEIGHT = 800;
