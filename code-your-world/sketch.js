900a47e0d07d0e6089afd18302987df903f5fec2
FAL <falworks.contact@gmail.com>
2020-05-31 17:37:05 +0900

    fix

--- code-your-world/sketch.js
+++ code-your-world/sketch.js
@@ -37,7 +37,7 @@
 
 // ----------------------------------------------------------------
 
-const scaleFactor = 4;
+const scaleFactor = 0.75;
 
 const 出でよ空 = () => {
   createCanvas(scaleFactor * 東西, scaleFactor * 南北);
